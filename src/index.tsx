5bc5e39f60a6bec03ae1179a352f4b63064d0dee
cidkumagai <f20aa003@gmail.com>
2023-05-14 19:03:36 +0900

    chore: 不要なライブラリの削除

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import './index.css';
 import App from './App';
@@ -17,7 +16,6 @@ root.render(
   <React.StrictMode>
       <QueryClientProvider client={queryClient}>
         <App />
-        <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
   </React.StrictMode>
 );
